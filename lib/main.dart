9f8a68313149bbe5d100cb3f5b4da8b653f5bb12
th-guasti <th.guasti@gmail.com>
2024-02-08 16:13:03 -0300

    explicação Stack, width, height, color (no stack é possivel colocar um elemento na frente de outro)

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,18 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(
-        color: Colors.white,
-        child: Container(color: Colors.blue),
-      ),
+      home: Stack(alignment: AlignmentDirectional.center, children: [
+        Container(
+          color: Colors.red,
+          width: 100,
+          height: 100,
+        ),
+        Container(
+          color: Colors.blue,
+          width: 50,
+          height: 50,
+        )
+      ]),
     );
   }
 }
